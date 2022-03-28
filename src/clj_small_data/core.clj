d41802df10c52c412772203c70cc7bf5d923ff9c
geekarist <geekarist@users.noreply.github.com>
2022-03-28 22:48:07 +0200

    Display fake view

--- src/clj_small_data/core.clj
+++ src/clj_small_data/core.clj
@@ -1,9 +1,31 @@
-(ns clj-small-data.core)
+(ns clj-small-data.core
+  (:require [cljfx.api :as fx]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn root [{:keys [showing]}]
+  {:fx/type :stage
+   :showing showing
+   :title "Hello"
+   :width 300
+   :height 100
+   :scene
+   {:fx/type :scene
+    :root
+    {:fx/type :v-box
+     :alignment :top-left
+     :children
+     [{:fx/type :text-field
+       :v-box/margin {:bottom 8 :left 8 :right 8}
+       :text "Hello again 🙂"}
+      {:fx/type :label
+       :v-box/margin {:bottom 8 :left 8 :right 8}
+       :text "First result"}
+      {:fx/type :label
+       :v-box/margin {:bottom 8 :left 8 :right 8}
+       :text "Second result"}
+      {:fx/type :label
+       :v-box/margin {:left 8 :right 8}
+       :text "Third result"}]}}})
 
 (comment
-  (foo "Well"))
+  (let [renderer (fx/create-renderer)]
+    (renderer {:fx/type root :showing true})))
