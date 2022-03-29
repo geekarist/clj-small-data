167e3aeb1c156253ca61ebf5db53767e355ca881
geekarist <geekarist@users.noreply.github.com>
2022-03-29 16:52:04 +0200

    Hold state in atom, iterative workflow

--- src/clj_small_data/core.clj
+++ src/clj_small_data/core.clj
@@ -1,31 +1,50 @@
 (ns clj-small-data.core
   (:require [cljfx.api :as fx]))
 
-(defn root [{:keys [showing]}]
+(def initial-state-map
+  {:title "Hello"
+   :search-field-placeholder "Please enter your search text"
+   :results
+   [{:text "First result"}
+    {:text "Second result"}
+    {:text "Third result"}
+    {:text "Fourth result"}
+    {:text "Fifth result"}
+    {:text "Sixth result"}
+    {:text "Seventh result"}]})
+
+(def state-atom
+  (atom initial-state-map))
+
+(defn root [{state-map :state}]
   {:fx/type :stage
-   :showing showing
-   :title "Hello"
-   :width 300
-   :height 100
+   :showing true
+   :title (state-map :title)
+   :width 600
+   :height 600
    :scene
    {:fx/type :scene
     :root
     {:fx/type :v-box
      :alignment :top-left
      :children
-     [{:fx/type :text-field
-       :v-box/margin {:bottom 8 :left 8 :right 8}
-       :text "Hello again 🙂"}
-      {:fx/type :label
-       :v-box/margin {:bottom 8 :left 8 :right 8}
-       :text "First result"}
-      {:fx/type :label
-       :v-box/margin {:bottom 8 :left 8 :right 8}
-       :text "Second result"}
-      {:fx/type :label
+     (cons
+      {:fx/type :text-field
        :v-box/margin {:left 8 :right 8}
-       :text "Third result"}]}}})
+       :text (state-map :search-field-placeholder)}
+      (map (fn [result-map]
+             {:fx/type :label
+              :v-box/margin {:top 8 :left 8 :right 8}
+              :text (result-map :text)})
+           (state-map :results)))}}})
+
+(def renderer
+  (fx/create-renderer
+   :middleware (fx/wrap-map-desc
+                (fn [state-val]
+                  {:fx/type root :state state-val}))))
+
+(fx/mount-renderer state-atom renderer)
 
 (comment
-  (let [renderer (fx/create-renderer)]
-    (renderer {:fx/type root :showing true})))
+  (swap! state-atom (fn [_state-val] initial-state-map)))
