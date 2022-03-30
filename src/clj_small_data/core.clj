85e851fe4bd8d1490f1c7d3dfefc5bfa097e7d81
geekarist <geekarist@users.noreply.github.com>
2022-03-30 18:04:28 +0200

    Refactoring, implement state update (search text)

--- src/clj_small_data/core.clj
+++ src/clj_small_data/core.clj
@@ -1,7 +1,7 @@
 (ns clj-small-data.core
   (:require [cljfx.api :as fx]))
 
-(def initial-state-map
+(def finder-init
   {:title "Hello again!"
    :search-field-placeholder "Please enter your search text"
    :results
@@ -14,15 +14,15 @@
     {:text "7th result"}]})
 
 (def state-atom
-  (atom initial-state-map))
+  (atom finder-init))
 
-(defn reload-state []
+(defn reload-state! []
   (swap!
    state-atom
    (fn [_state-val]
-     initial-state-map)))
+     finder-init)))
 
-(defn root [{state-map :state}]
+(defn finder-view [{state-map :state dispatch! :dispatch}]
   {:fx/type :stage
    :showing true
    :title (state-map :title)
@@ -43,10 +43,11 @@
          :style {:-fx-background-color "#ff0000"}
          :h-box/hgrow :always
          :h-box/margin {:right 8}
-         :prompt-text (state-map :search-field-placeholder)}
+         :prompt-text (state-map :search-field-placeholder)
+         :on-text-changed #(dispatch! [:change-search-query %])}
         {:fx/type :button :text "Search" :h-box/margin {:right 8}}
         {:fx/type :button :text "Reload"
-         :on-action (fn [_] (reload-state))}]}
+         :on-action (fn [_] (reload-state!))}]}
       (map (fn [result-map]
              {:fx/type :label
               :v-box/margin {:top 16}
@@ -54,13 +55,24 @@
               :text (result-map :text)})
            (state-map :results)))}}})
 
+(defn finder-update [state-hash msg-key msg-val]
+  (condp = msg-key
+    :change-search-query
+    (assoc state-hash :search-text msg-val)))
+
+(defn dispatch! [[msg-key msg-val :as _message-vec]]
+  (let [new-state-hash (finder-update @state-atom msg-key msg-val)
+        get-new-state-hash (fn [_current-state-hash] new-state-hash)]
+    (swap! state-atom get-new-state-hash)))
+
 (def renderer
   (fx/create-renderer
    :middleware (fx/wrap-map-desc
                 (fn [state-val]
-                  {:fx/type root :state state-val}))))
+                  {:fx/type finder-view :state state-val :dispatch dispatch!}))))
 
 (fx/mount-renderer state-atom renderer)
 
 (comment
-  (reload-state))
+  (println @state-atom)
+  (reload-state!))
