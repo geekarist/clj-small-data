361055a0a0470d685e40dc4f02e95b7b888ea0c3
geekarist <geekarist@users.noreply.github.com>
2022-03-31 18:41:03 +0200

    Reorder buttons, add log effect

--- src/clj_small_data/finder.clj
+++ src/clj_small_data/finder.clj
@@ -19,15 +19,16 @@
       {:fx/type :h-box :style {:-fx-background-color "#00ff00"}
        :children
        [{:fx/type :text-field :style {:-fx-background-color "#ff0000"}
-         :h-box/hgrow :always :h-box/margin {:right 8}
-         :text (state-map :mdl/search-text)
+         :h-box/hgrow :always :text (state-map :mdl/search-text)
          :prompt-text (state-map :mdl/search-field-placeholder)
          :on-text-changed #(dispatch! [:msg/change-search-query %])}
-        {:fx/type :button :text "Search"}
+        {:fx/type :button :text "Clear"
+         :on-action (fn [_] (dispatch! [:msg/clear]))}
+        {:fx/type :button :text "Search" :h-box/margin {:left 8}}
         {:fx/type :button :text "Redraw" :h-box/margin {:left 8}
          :on-action (fn [_] (dispatch! [:msg/redraw]))}
-        {:fx/type :button :text "Clear" :h-box/margin {:left 8}
-         :on-action (fn [_] (dispatch! [:msg/clear]))}]}
+        {:fx/type :button :text "Log"
+         :on-action (fn [_] (dispatch! [:msg/log]))}]}
       (map (fn [result-map]
              {:fx/type :label :v-box/margin {:top 16}
               :style {:-fx-background-color "#0000ff"}
@@ -35,22 +36,32 @@
            (state-map :mdl/results)))}}})
 
 (defn update [state-hash msg-key msg-val]
+
   (condp = msg-key
+
     :msg/change-search-query
     (let [new-state-hash (assoc state-hash :mdl/search-text msg-val)
           new-effect-vec nil]
       [new-state-hash new-effect-vec])
+
     :msg/redraw
     (let [new-state-hash state-hash
           new-effect-vec nil]
       [new-state-hash new-effect-vec])
+
     :msg/clear
     (let [new-state-hash init
           new-effect-vec nil]
+      [new-state-hash new-effect-vec])
+
+    :msg/log
+    (let [new-state-hash state-hash
+          new-effect-vec [:fx/log state-hash]]
       [new-state-hash new-effect-vec])))
 
-(defn effect! [[key _value :as _new-effect-vec] _dispatch!]
-  #_(condp = key
-      :fx/reload-initial-state
-      (reload-state!)))
+(defn effect! [[key value :as _new-effect-vec] _dispatch!]
+  (condp = key
+    :fx/log (println "State:" value)
+    nil nil ; Ignore `nil` effect
+    (println "Effect not found:" key)))
 
