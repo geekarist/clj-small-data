401aad7f0a4b62a639418cad46372280d606c9de
geekarist <geekarist@users.noreply.github.com>
2022-04-28 08:37:13 +0200

    Use wt to open URI

--- src/clj_uri/core.clj
+++ src/clj_uri/core.clj
@@ -10,7 +10,7 @@
              ps-script-file (File/createTempFile "clj-uri-open" ".ps1")
              ps-script-path-str (-> ps-script-file (.getPath))
              _ (spit ps-script-file ps-cmd-str)
-             cmd-resp (shell/sh "powershell.exe" "-Command" ps-script-path-str)]
+             cmd-resp (shell/sh "wt" "PowerShell" ps-script-path-str)]
          (timbre/debug "PowerShell script-path" (-> ps-script-file (.getPath)))
          (timbre/debug "Command response:" cmd-resp))
        (catch Exception e
@@ -24,5 +24,4 @@
   (open! "file:///c:/program%20files")
   (open! "mailto:cpele@molotov.tv")
   (open! "mailto:cpele+test2@molotov.tv")
-  (shell/sh "powershell" "C:\\Users\\chris\\AppData\\Local\\Temp\\clj-uri-open10673641866898109012.ps1")
   (comment))
